3f527fe497545b62c524f54dac3047cb402fcbfa
Mingzhe Zou <zoumingzhe@outlook.com>
2026-01-11 11:46:38 +0800

    setup.py: comment out MIT License classifier
    See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14",
-        "License :: OSI Approved :: MIT License",
+        # "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
